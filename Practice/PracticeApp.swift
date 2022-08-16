a817cc5422c4ec4c658591be1d51b6270675b269
Anika Yadav <aanikayadav@gmail.com>
2022-08-16 14:00:59 -0500

    Initial Commit

---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,18 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by scholar on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
